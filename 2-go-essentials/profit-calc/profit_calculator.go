2b4d8b4bf7685616e1d826ed061a4488bc039f54
Aurimas Ragulis <aurimas.ragulis@vinted.com>
2025-08-21 13:42:26 +0300

    feat: error handling & file writing exercise

--- 2-go-essentials/profit-calc/profit_calculator.go
+++ 2-go-essentials/profit-calc/profit_calculator.go
@@ -1,23 +1,61 @@
 package main
 
-import "fmt"
+import (
+	"errors"
+	"fmt"
+	"os"
+)
+
+const resultsFile string = "financials.txt"
 
 func main() {
-	revenue := getUserInput("Revenue: ")
-	expenses := getUserInput("Expenses: ")
-	taxRate := getUserInput("Tax Rate (%): ")
+	revenue, err := getUserInput("Revenue: ")
+	if err != nil {
+		processError(err)
+		return
+	}
+	expenses, err := getUserInput("Expenses: ")
+	if err != nil {
+		processError(err)
+		return
+	}
+	taxRate, err := getUserInput("Tax Rate (%): ")
+	if err != nil {
+		processError(err)
+		return
+	}
 
 	earningsBeforeTax, profit, ratio := calculateStats(revenue, expenses, taxRate)
 
-	fmt.Printf("EBT: %.2f\n", earningsBeforeTax)
-	fmt.Printf("Profit: %.2f\n", profit)
-	fmt.Printf("Ratio: %.2f\n", ratio)
+	results := fmt.Sprintf(`
+	EBT: %.2f
+	Profit: %.2f
+	Ratio: %.2f
+	`, earningsBeforeTax, profit, ratio)
+	writeToFile(results, resultsFile)
 }
 
-func getUserInput(prompt string) (input float64) {
+func getUserInput(prompt string) (input float64, err error) {
 	fmt.Print(prompt)
 	fmt.Scan(&input)
-	return
+	if input <= 0 {
+		return 0, errors.New("input must be a positive number")
+	}
+	return input, nil
+}
+
+func processError(err error) {
+	fmt.Println("=====")
+	fmt.Println("An error occured:", err)
+	fmt.Println("Exiting...")
+	fmt.Println("=====")
+}
+
+func writeToFile(text string, fileName string) {
+	err := os.WriteFile(resultsFile, []byte(text), 0644)
+	if err != nil {
+		processError(err)
+	}
 }
 
 func calculateStats(revenue, expenses, tax float64) (ebt, profit, ratio float64) {
